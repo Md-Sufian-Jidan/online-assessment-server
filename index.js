2f3a6490bbeb8f2a7fc414c1c26a94c8cef5f255
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-04-07 11:02:34 +0600

    delete a single assignment api created

--- index.js
+++ index.js
@@ -56,12 +56,16 @@ async function run() {
             const filter = { _id: new ObjectId(id) };
             const result = await assignmentCollection.deleteOne(filter);
             res.send(result);
-        })
+        });
+
+        app.update('/update-assignment/:id', async (req, res) => {
+            const id = req.params.id;
+        });
 
         app.get('/features', async (req, res) => {
             const result = await featureCollection.find().toArray();
             res.send(result);
-        })
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
