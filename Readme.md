2eee32102860a5c0f0a094755325617d00003e80
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-04-08 12:19:19 +0600

    project deploy and readme.md file added

---
+++ Readme.md
@@ -0,0 +1,28 @@
+**Backend:**
+- Node.js
+- Express.js
+- MongoDB
+- JWT
+- CORS
+- Cookies
+
+```bash
+git clone https://github.com/Md-Sufian-Jidan/online-assessment-server.git
+cd online-assessment-server
+
+npm install
+
+PORT=5000
+MONGO_URI=your_mongodb_connection_string
+JWT_SECRET=your_secret_key
+
+npm run start
+
+- The API will be running at:
+
+http://localhost:5000
+
+# Build
+npm run build
+
+# Deploy with your platform (Vercel, Render, etc.)
--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const port = process.env.port || 5000;
 
 //middlewares
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+        "http://localhost:5173",
+        "https://online-assessment-client.web.app",
+    ],
     credentials: true
 }));
 app.use(express.json());
@@ -56,37 +59,37 @@ const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production" ? true : false,
     sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-}
+};
 
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const assignmentCollection = client.db("study-sync").collection("assignments");
         const submittedAssignmentCollection = client.db("study-sync").collection("submitted-assignments");
         const featureCollection = client.db("study-sync").collection("features");
 
         // jwt authentication
-        // app.post('/jwt', logger, async (req, res) => {
-        //     const user = req.body;
-        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-        //     res
-        //         .cookie('token', token, {
-        //             httpOnly: true,
-        //             secure: false,
-        //             sameSite: 'none'
-        //         })
-        //         .send({ success: true });
-        // });
+        app.post('/jwt', logger, async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,
+                    sameSite: 'none'
+                })
+                .send({ success: true });
+        });
 
-        // app.post('/logout', logger, async (req, res) => {
-        //     const user = req.body;
-        //     console.log('logging user', user);
-        //     // maxAge = 0 means expire the token
-        //     res.clearCookie('token', { ...cookieOptions, maxAge: 0 })
-        //         .send({ success: true })
-        // });
+        app.post('/logout', logger, async (req, res) => {
+            const user = req.body;
+            console.log('logging user', user);
+            // maxAge = 0 means expire the token
+            res.clearCookie('token', { ...cookieOptions, maxAge: 0 })
+                .send({ success: true });
+        });
 
         // assignment create api
         app.post('/create-assignment', logger, async (req, res) => {
@@ -94,6 +97,7 @@ async function run() {
             const result = await assignmentCollection.insertOne(assignment);
             res.send(result);
         });
+
         // assignments get api
         app.get('/assignments', logger, async (req, res) => {
             const page = parseInt(req.query.page) || 1;
@@ -188,15 +192,6 @@ async function run() {
         app.get('/leaderboard', async (req, res) => {
             const result = await submittedAssignmentCollection.find().toArray();
             res.send(result);
-        });
-
-        // app.get('/pagination', async (req, res) => {
-
-        // });
-
-        app.get('/count-assignments', async (req, res) => {
-            const count = await assignmentCollection.estimatedDocumentCount();
-            res.send(count);
         });
 
         app.get('/features', logger, async (req, res) => {
